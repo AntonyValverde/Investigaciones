78f154e9b25e97464719e5b6dba7274172567d72
Antony Valverde R <gatovalverde4@gmail.com>
2024-08-30 14:40:58 -0600

    App

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import doc3 from './Documents/Contaminación del agua por metales pesados.pdf';
 import doc4 from './Documents/Presentación sobre diseños de investigación.pdf';
 import doc5 from './Documents/Uso de la Inteligencia artificial en el desarrollo laboral en las Pymes de Costa Rica del 2021 - 2023(1).pdf';
 import doc6 from './Documents/Uso de la Inteligencia artificial en el desarrollo laboral en las Pymes de Costa Rica del 2021 - 2023 (3).pdf';
-
+import doc7 from './Documents/Librerías informáticas utilizadas en análisis de imágenes dermatológicas con visión computacional en las áreas de la medicina y sociedad.pdf';
 function App() {
   const sintesis = [
     { title: 'Síntesis 1', description: 'Sociedad de la Información, Sociedad Digital, Sociedad de Control', url: doc1 },
@@ -22,6 +22,7 @@ function App() {
     { title: 'Investigación 1', description: 'Instituto Mixto de Ayuda Social (IMAS), para combatir la desigualdad en acceso a la tecnología en la sociedad de Costa Rica 2019-2024', url: doc2 },
     { title: 'Investigación 2', description: 'Presentación sobre diseños de investigación.', url: doc4 },
     { title: 'Investigación 3', description: 'Uso de la Inteligencia artificial en el desarrollo laboral en las Pymes de Costa Rica del 2021 - 2023', url: doc6 },
+    { title: 'Investigación 4', description: 'Librerías informáticas utilizadas en análisis de imágenes dermatológicas con visión computacional en las áreas de la medicina y sociedad', url: doc7 },
   ];
 
   const articulos = [
