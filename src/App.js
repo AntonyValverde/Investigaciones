7afae0c49509c8fb8e9f20e85617c0d94d4f3f4d
Antony Valverde R <gatovalverde4@gmail.com>
2024-09-12 09:44:53 -0600

    Investigacion5

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ import doc4 from './Documents/Presentación sobre diseños de investigación.pdf
 import doc5 from './Documents/Uso de la Inteligencia artificial en el desarrollo laboral en las Pymes de Costa Rica del 2021 - 2023(1).pdf';
 import doc6 from './Documents/Uso de la Inteligencia artificial en el desarrollo laboral en las Pymes de Costa Rica del 2021 - 2023 (3).pdf';
 import doc7 from './Documents/Librerías informáticas utilizadas en análisis de imágenes dermatológicas con visión computacional en las áreas de la medicina y sociedad.pdf';
+import doc8 from './Documents/LagunaJordan_ValverdeRojas_Trabajo_Investigación^N3.pdf';
+
+
 function App() {
   const sintesis = [
     { title: 'Síntesis 1', description: 'Sociedad de la Información, Sociedad Digital, Sociedad de Control', url: doc1 },
@@ -23,8 +26,9 @@ function App() {
     { title: 'Investigación 2', description: 'Presentación sobre diseños de investigación.', url: doc4 },
     { title: 'Investigación 3', description: 'Uso de la Inteligencia artificial en el desarrollo laboral en las Pymes de Costa Rica del 2021 - 2023', url: doc6 },
     { title: 'Investigación 4', description: 'Librerías informáticas utilizadas en análisis de imágenes dermatológicas con visión computacional en las áreas de la medicina y sociedad', url: doc7 },
+    { title: 'Investigación 5', description: 'Implementación de la Tecnología Digital para el desarrollo de habilidades en la educación costarricense 2019-2024', url: doc8 },
   ];
-
+  
   const articulos = [
     { title: 'Artículo científico 1', description: 'Uso de la Inteligencia artificial en el desarrollo laboral en las Pymes de Costa Rica del 2021 - 2023', url: doc5 },
   ];
